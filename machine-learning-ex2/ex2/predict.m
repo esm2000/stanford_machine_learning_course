4054a585a5de7f05997e81812eb5299c5e41063f
Emeka Mbazor <47117694+esm2000@users.noreply.github.com>
2020-02-21 16:40:05 -0500

    implement predict() function that classifies input

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,11 +15,15 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+hypothesis = sigmoid( X * theta ); 
 
-
-
-
-
+for i = 1:m
+    if hypothesis(i,1) < 0.5
+        p(i, 1) = 0;
+    else 
+        p(i, 1) = 1;
+    endif;
+endfor;
 
 % =========================================================================
 
