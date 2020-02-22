718099343cabf845c3be575fac5a52ca28bbc172
Emeka Mbazor <47117694+esm2000@users.noreply.github.com>
2020-02-21 23:52:06 -0500

    implement logistic regression gradient descent with regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,8 +18,29 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+% zero
 
+theta_zero = theta(1,1);
+X_zero = X(:, 1);
 
+% rest
+
+theta_rest = theta(2:end, 1);
+X_rest = X(:, 2:end)
+
+% calculate the hypotheses
+
+hypothesis = sigmoid( X * theta );
+
+% calculate cost
+ 
+J = ( 1 / m ) * sum( ( -y .* log( hypothesis ) ) - ( ( 1 - y ) .* log( 1 - hypothesis )) )
+J = J + ( ( lambda / ( 2 * m ) ) * sum( theta_rest .^ 2 )  ) 
+
+grad_zero = ( 1 / m ) * sum( (hypothesis - y) .* X_zero );
+grad_rest = ( ( 1 / m ) * sum( ( hypothesis - y ) .* X_rest ) )' + ( ( lambda / m ) .* theta_rest );
+
+grad = [ grad_zero; grad_rest ];
 
 
 % =============================================================
